dc720291209c4c1950064a81b800cd4306472b2b
Vurhd420 <dhruv420@gmail.com>
2020-07-16 15:54:44 -0500

    Button Tests
    Having a UI and a button turn it off

--- All in a Day's Work/Assets/Scripts/RoomUIoff.cs
+++ All in a Day's Work/Assets/Scripts/RoomUIoff.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RoomUIoff : MonoBehaviour
 {
+    public int roomamount = 4;
+    public Button CreateRoom;
     // Start is called before the first frame update
     void Start()
     {
-        
+        CreateRoom = GameObject.Find("CreateRoom").GetComponent<Button>();
+        CreateRoom.onClick.AddListener(delegate(){ TaskOnClick("urdumb"); });
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+   
+    void TaskOnClick(string debug)
+    {
+        Debug.Log(debug);
+        GameObject.Find("RoomUI").SetActive(false);
     }
 }
